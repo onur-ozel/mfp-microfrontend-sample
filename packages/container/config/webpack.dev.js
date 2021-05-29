4780f0a6546c33054950f666ef500a08b047ed69
onur-ozel <onurozel41@gmail.com>
2021-05-29 19:34:47 +0300

    container ci/cd pipelines

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const ModuleFedarationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFedarationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
